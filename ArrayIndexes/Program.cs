90b54d0964693d87a5e0edbc951e46fc8a33dda2
Natalia <besarabnatalia777@gmail.com>
2023-01-26 22:07:03 +0300

    26.01 Arrays

---
+++ ArrayIndexes/Program.cs
@@ -0,0 +1,19 @@
+﻿//How to get last element of array
+
+int[] myArray = { 2, 8, 6, 20, 56, 77, 98, 100 };
+//Console.WriteLine(myArray[myArray.Length -1]);
+//Console.WriteLine(myArray[^1]);
+//int[] myArray1 = myArray[..5];
+
+int[] myArray2 = myArray[4..];
+
+//Index
+
+int[] newArray = { 2, 8, 6, 20, 56, 77, 98, 100 };
+Index myIndex = ^3;
+Console.WriteLine(newArray[myIndex]);
+Console.WriteLine($"value: {myIndex.Value} isFromEnd: {myIndex.IsFromEnd}");
+
+string word = "Hello World!=)";
+Console.WriteLine(word[^2..]);
+
--- ArraysMinValue/Program.cs
+++ ArraysMinValue/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+
 //Find minValue in Array
 
 int[] myArray = { 2, 6, 9, 10, 12, 889, 76, 56, 45, 80 };
@@ -16,8 +17,6 @@ Console.WriteLine(minValue);
 
 //Find maxValue in Array
 
-
-
 int maxValue = myArray[0];
 
 for (int i = 1; i < myArray.Length; i++)
@@ -34,10 +33,17 @@ int[] mineArray = { 111, 10, 4, 99,111, 47, 69, 77,99, 5, 25, 5, 89 };
 int[] result = mineArray.Distinct().ToArray();
 Console.WriteLine(result);
 
+//Console.WriteLine(mineArray.Max());
+//Console.WriteLine(mineArray.Min());
+//Console.WriteLine(mineArray.Sum());
+//Console.WriteLine(mineArray.Where(i => i % 2 == 0).Sum());
+////Sum of even numbers
+//Console.WriteLine(mineArray.Where(i => i % 2 != 0).Min());
 
-Console.WriteLine(mineArray.Max());
-Console.WriteLine(mineArray.Min());
-Console.WriteLine(mineArray.Sum());
-Console.WriteLine(mineArray.Where(i => i % 2 == 0).Sum());
-//Sum of even numbers
-Console.WriteLine(mineArray.Where(i => i % 2 != 0).Min());
+//Continue
+int[] firstArray = { 111, 10, 4, 99, 111, 47, 69, 77, 99, 5, 25, 5, 89 };
+Array.Sort(firstArray);
+Array.Reverse(firstArray);  
+int resultive = Array.Find(firstArray,i => i < 70);
+Console.WriteLine( firstArray );
+Console.WriteLine( resultive );
